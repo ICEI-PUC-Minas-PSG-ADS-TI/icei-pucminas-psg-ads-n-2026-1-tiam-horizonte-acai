ab798429c99aa0e47abb1a77e55ddc61ad83f4e3
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-05-17 23:17:16 -0300

    Revert "apagando uma pasta duplicada"
    This reverts commit 15dfcc47e2b0903941c3134af830dac55d8b89b7.

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
+<<<<<<< HEAD
 import { Platform } from 'react-native'
+=======
+>>>>>>> parent of 15dfcc4 (apagando uma pasta duplicada)
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
 
+<<<<<<< HEAD
 const storage = Platform.OS === 'web'
   ? {
       getItem: (key) => Promise.resolve(typeof window !== 'undefined' ? window.localStorage.getItem(key) : null),
@@ -19,5 +23,13 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: Platform.OS === 'web',
+=======
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+>>>>>>> parent of 15dfcc4 (apagando uma pasta duplicada)
   },
 })
