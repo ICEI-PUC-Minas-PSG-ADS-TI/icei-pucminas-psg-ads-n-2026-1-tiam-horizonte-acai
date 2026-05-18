15dfcc47e2b0903941c3134af830dac55d8b89b7
Samuel Vitor Silveira da Silva Silva <samuelvitor2802@gmail.com>
2026-05-17 22:59:05 -0300

    apagando uma pasta duplicada

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://anywbzflyrldirhsthjz.supabase.co'
-const supabaseKey = 'sb_publishable_XDhdlKxi6QtwtzqCBdeddQ_KN92LSTD'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
